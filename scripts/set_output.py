d668bfe0d892e262810ea1d2d406d966339da2ed
fynnbe <thefynnbe@gmail.com>
2021-12-08 23:45:55 +0100

    smth

--- scripts/set_output.py
+++ scripts/set_output.py
@@ -1,2 +1,12 @@
+import requests
+
 name = "out"
 print(f"::set-output name={name}::my output")
+
+
+r = requests.get(
+    f"https://zenodo.org/api/records/?&sort=mostrecent&page=1&size=1000&all_versions=0&keywords=bioimage.io"
+)
+print(r.status_code)
+print(r.reason)
+print(r.json())
